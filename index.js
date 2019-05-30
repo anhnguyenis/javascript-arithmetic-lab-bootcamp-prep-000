246427aa390c68009160a625127c59e946ca160d
Anh Nguyen <anhnguyenis@gmail.com>
2019-05-30 13:58:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ function subtract (a,b) {
   return b-a
 }
 
+function multiply (a,b) {
+  return a*b
+}
+
+function divide (a,b) {
+  return a/b
+}
+
+function inc(n) {
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
