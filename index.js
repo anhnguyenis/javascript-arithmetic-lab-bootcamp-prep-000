7b8b2d0690d14138059204b5a69b1bb3e17c1f29
Anh Nguyen <anhnguyenis@gmail.com>
2019-05-30 15:00:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function makeInt(n) {
   return parseInt(n);
 }
 
-function makeInt('n',10) {
-  return parseInt('n',10);
-}
+function makeInt(n, 10) {
+  var parsed = parseInt(x, base);
+  if (isNaN(parsed)) { return 0 }
+  return parsed * 100;
